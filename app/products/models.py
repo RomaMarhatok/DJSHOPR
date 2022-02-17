9640375410a340305c7aecc62450cc70dc27d9f1
RomaMarhatok <r.marhatok@yandex.by>
2022-02-17 17:54:26 +0300

    add: attribute model

--- app/products/models.py
+++ app/products/models.py
@@ -9,3 +9,13 @@ class Manufacturer(models.Model):
         verbose_name_plural = ("manufacturers")
     def __str__():
         return f"manufacturer:{name}"
+
+class Attribute(models.Model):
+    name = models.CharField(verbose_name = "full attribute name",max_length=255)
+    value = models.CharField(verbose_name = "value of attribute",max_length=255)
+    slug = models.CharField(verbose_name = f"slug for attribute:{name}",max_length=255)
+    class Meta:
+        verbose_name = ("attribute")
+        verbose_name_plural("attributes")
+    def __str__():
+        return f"attribute:{name}"
