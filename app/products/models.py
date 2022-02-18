0aaa7bfc9838258da42ba93d75fbf88ecc0a6b96
RomaMarhatok <r.marhatok@yandex.by>
2022-02-18 17:07:06 +0300

    add: product category model

--- app/products/models.py
+++ app/products/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Manufacturer(models.Model):
-    name = models.CharField(verbose_name = "full manufacturer name",max_length = 255)
-    slug = models.CharField(verbose_name = f"slug for manufacturer:{name}",max_length=255) # can do this ?
+    name = models.CharField(verbose_name = "full manufacturer name",max_length = 255,unique = True)
+    slug = models.CharField(verbose_name = f"slug for manufacturer:{name}",max_length=255, unique = True)
     class Meta:
         verbose_name = ("manufacturer")
         verbose_name_plural = ("manufacturers")
@@ -13,9 +13,19 @@ class Manufacturer(models.Model):
 class Attribute(models.Model):
     name = models.CharField(verbose_name = "full attribute name",max_length=255)
     value = models.CharField(verbose_name = "value of attribute",max_length=255)
-    slug = models.CharField(verbose_name = f"slug for attribute:{name}",max_length=255)
+    slug = models.CharField(verbose_name = f"slug for attribute:{name}",max_length=255, unique = True)
     class Meta:
         verbose_name = ("attribute")
-        verbose_name_plural("attributes")
+        verbose_name_plural = ("attributes")
     def __str__():
         return f"attribute:{name}"
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name = "full category name", max_length=255)
+    slug = models.CharField(verbose_name = "slug for product",max_length=255, unique = True)
+    class Meta:
+        verbose_name = ('product category name')
+        verbose_name_plural = ('product category names')
+    def __str__():
+        return f"Product category:{name}"
+
