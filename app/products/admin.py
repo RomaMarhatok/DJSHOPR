9433463bf5e57be94e2ae69938c13615d7f465b8
RomaMarhatok <r.marhatok@yandex.by>
2022-02-20 22:11:19 +0300

    add: models in admin panel

--- app/products/admin.py
+++ app/products/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
-
+from products.models import *
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Product,ProductAdmin)
+class ManufacturerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Manufacturer,ManufacturerAdmin)
+class AttributeAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Attribute,AttributeAdmin)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(ProductCategory,ProductCategoryAdmin)
+
+
+
+
+admin.site.register(ProductAttribute)
