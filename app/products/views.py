f2d051718ea3b101ed248ca549a30f5b3bdfcf22
RomaMarhatok <r.marhatok@yandex.by>
2022-02-22 20:46:11 +0300

    create: index and view views , add app in urls

--- app/products/views.py
+++ app/products/views.py
@@ -1,7 +1,16 @@
+from itertools import product
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from products.models import Product,ProductCategory,Manufacturer,ProductAttribute
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    products = Product.objects.values()
+    for i in products:
+        i['manufacturer'] = Manufacturer.objects.get(pk = i['manufacturer_id_id'])
+    attributes = products[0].keys()
+    return render(request,'index.html',context={'attributes':attributes,'products':products})
+
+def view(request,slug):
+    print(slug)
+    return HttpResponse('hello')
