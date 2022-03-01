eaa9f79ff849009ad016309dadb32116030fb173
roma <roma.marhatok@yandex.by>
2022-03-01 07:14:09 +0300

    add: filter for product and change clean methods for form

--- app/products/forms.py
+++ app/products/forms.py
@@ -1,39 +1,43 @@
-from typing import Dict,Any
+from typing import Dict, Any
 from django.core.validators import MinValueValidator
 from django import forms
-from products.models import Manufacturer,ProductCategory,Product
+from products.models import Manufacturer, ProductCategory, Product
+
 
 class ProductFilterForm(forms.Form):
     STATUS_CHOICES = (
-        ("-",("-")),
-        ("phone",("phone")),
-        ("notebook",("notebook")),
-        ("monitor",("monitor")),
-        ("headphones",("headphones"))
+        ("-", ("-")),
+        ("phone", ("phone")),
+        ("notebook", ("notebook")),
+        ("monitor", ("monitor")),
+        ("headphones", ("headphones"))
     )
     ORDER_CHOICES = (
-        ("-",("-")),
-        ("name",("name")),
-        ("price",("price")),
-        ("-name",("reverse name")),
-        ("-price",("reverse price"))
+        ("-", ("-")),
+        ("name", ("name")),
+        ("price", ("price")),
+        ("-name", ("reverse name")),
+        ("-price", ("reverse price"))
     )
-    category = forms.ChoiceField(choices=STATUS_CHOICES,widget=forms.Select(attrs={'class':'form-select'}),required=False)
-    order_by = forms.ChoiceField(choices=ORDER_CHOICES,widget=forms.Select(attrs={'class':'form-select'}),required=False)
+    category = forms.ChoiceField(choices=STATUS_CHOICES, widget=forms.Select(
+        attrs={'class': 'form-select'}), required=False)
+    order_by = forms.ChoiceField(choices=ORDER_CHOICES, widget=forms.Select(
+        attrs={'class': 'form-select'}), required=False)
     max_price = forms.IntegerField(required=False)
     min_price = forms.IntegerField(required=False)
 
     def clean_max_price(self):
-        data = -1 if self.cleaned_data['max_price'] is None else self.cleaned_data['max_price']
-        return data 
+        data = None if self.cleaned_data['max_price'] is None else self.cleaned_data['max_price']
+        return data
 
     def clean_min_price(self):
-        data = -1 if self.cleaned_data['min_price'] is None else self.cleaned_data['min_price']
+        data = None if self.cleaned_data['min_price'] is None else self.cleaned_data['min_price']
         return data
 
     def clean_category(self):
-        query_set_catetory = ProductCategory.objects.filter(name = self.cleaned_data['category'])
-        if len(query_set_catetory)==0:
+        query_set_catetory = ProductCategory.objects.filter(
+            name=self.cleaned_data['category'])
+        if len(query_set_catetory) == 0:
             category_name = None
         else:
             category_name = query_set_catetory[0]
@@ -41,8 +45,7 @@ class ProductFilterForm(forms.Form):
 
     def clean_order_by(self):
         order_by = self.cleaned_data['order_by']
-        for name,order in self.ORDER_CHOICES:
-            if order_by == name and order_by!="-":
+        for name, order in self.ORDER_CHOICES:
+            if order_by == name and order_by != "-":
                 return name
         return None
-        
