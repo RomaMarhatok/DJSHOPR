05446606777e25f7e347b6c94c9eb3e770ad7169
RomaMarhatok <r.marhatok@yandex.by>
2022-03-15 19:42:11 +0300

    add models in admin panel

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from user.models import *
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Basket)
+admin.site.register(BasketProduct)
+admin.site.register(Order)
+
