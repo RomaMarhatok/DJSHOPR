b5d1b1f10ed8145e88b17b1916b9d9aa309e78b9
RomaMarhatok <r.marhatok@yandex.by>
2022-03-16 16:58:33 +0300

    deleted user model

--- app/user/admin.py
+++ app/user/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from user.models import *
-# Register your models here.
 
-admin.site.register(User)
 admin.site.register(Basket)
 admin.site.register(BasketProduct)
 admin.site.register(Order)
