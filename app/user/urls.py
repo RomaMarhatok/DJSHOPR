6343dd156321804391ab639e892dcfd385938da4
RomaMarhatok <r.marhatok@yandex.by>
2022-03-21 08:56:26 +0300

    created urls for registration

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from user.views import RegistrationView
+from user.views import RegistrationView, SingInView,logout_user
 
 urlpatterns = [
-    path("registration/", RegistrationView.as_view(), name="registration")
-    ]
+    path("registration/", RegistrationView.as_view(), name="registration"),
+    path("sign in/", SingInView.as_view(), name="sign_in"),
+    path("logout/",logout_user,name="logout"),
+    
+]
