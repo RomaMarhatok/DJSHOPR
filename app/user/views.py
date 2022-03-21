8be8eab9fdb75f86bed63a8a23407522e487ff83
RomaMarhatok <r.marhatok@yandex.by>
2022-03-21 08:55:30 +0300

    created views for  registration

--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views import View
-from user.forms import RegistrationForm
+from django.views.generic import ListView
+from user.forms import RegistrationForm, SingInForm
 from django.contrib.auth.models import User
+from django.contrib.auth.views import LoginView
+from user.models import BasketProduct,Basket
+from products.models import Product
+from django.contrib.auth import logout,login
+# from django.contrib.auth.validator
 
 
 class RegistrationView(View):
@@ -14,5 +21,47 @@ class RegistrationView(View):
         return render(request, self.template_name, context={"form": self.form_class})
 
     def post(self, request):
-        d2 = {key: value[0] for key, value in request.POST.items()}
-        return HttpResponse(d2)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username", None)
+            password = form.cleaned_data.get("password", None)
+            email = form.cleaned_data.get("email", None)
+            user = User.objects.create_user(
+                username=username, password=password, email=email
+            )
+            Basket.objects.create(user__id=User.objects.filter(username=user)[0].pk)
+            return reverse_lazy("sign_in")
+        else:
+            return render(request, self.template_name, context={"form": form})
+
+
+class SingInView(LoginView):
+    template_name = "user/registration/sign_in.html"
+    form_class = SingInForm
+    model = User
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['form'] = self.form_class
+        return context_data
+    
+    def get_success_url(self) -> str:
+        return reverse_lazy('index')
+
+class BasketListView(ListView):
+    template_name = "user/basket.html"
+    model = BasketProduct
+    context_object_name = "products"
+
+    def get_queryset(self):
+        user_pk = User.objects.filter(username = self.request.user)[0].pk
+        basket = Basket.objects.filter(user__id=user_pk)
+        basketproducts_queryset = BasketProduct.objects.filter(basket__id = basket[0].pk)
+        products_name = [basketproduct.product for basketproduct in basketproducts_queryset ]
+        products_name
+        product_queryset = Product.objects.filter(name__in=products_name)
+        return product_queryset
+
+def logout_user(request):
+    logout(request)
+    return redirect('sign_in')
